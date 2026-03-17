304d366126b4fe4fd564d25e16164f53d66dade2
PureJuly <mymin8724@gmail.com>
2026-03-17 16:01:54 +0900

    ros2 cpp version 2

--- src/src/dist_turtle_action_server.cpp
+++ src/src/dist_turtle_action_server.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 #include "rclcpp.h"
 #include "turtlesim.h"
 #include "geometry_msgs.h"
@@ -19,4 +20,23 @@ using rcl_interfaces::msg;
 
 #include <math.h>
 #include <time.h>
+
+class TurtleSub_Action: public TurtlesimSubscriber 
+{
+  public:
+    TurtleSub_Action(ac_server)
+    {
+      this->ac_server = ac_server;
+    }
+
+  void callback(msg)
+    {
+      this->ac_server.current_pose = msg;
+    }
+};
+
+class DistTurtleServer: public Node
+{
+  
+}
 
